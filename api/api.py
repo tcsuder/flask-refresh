618c3674518ea7e668dddacb528f468f114b72c6
tcsuder@gmail.com <tcsuder@gmail.com>
2020-04-05 18:50:18 -0700

    setup working api call with dev proxy to flask back end

--- api/api.py
+++ api/api.py
@@ -1,10 +1,13 @@
 import time
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    return {
-        time: time.time()
-    }
+    print('testing testing 123')
+    return {'time': time.time()}
+
+@app.route('/')
+def index():
+    return 'testin'
